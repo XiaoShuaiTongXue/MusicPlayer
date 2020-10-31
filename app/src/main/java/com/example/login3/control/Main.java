a843647b4e8c4b467bae9cbb4e4aa8363fee8b07
xiaoshuai <2236065790@qq.com>
2020-10-31 15:26:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/login3/control/Main.java
@@ -0,0 +1,23 @@
+package com.example.login3.control;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.login3.R;
+
+public class Main extends AppCompatActivity {
+
+    private TextView user,psw;
+    private CheckBox cb;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //InitView();
+    }
+}
